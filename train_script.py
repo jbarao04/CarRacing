023a859724ce685ecc3cb73a6e257b0cbf8810a8
João Barão <114247064+jbarao04@users.noreply.github.com>
2024-12-09 16:54:43 +0000

    Add files via upload

--- train_script.py
+++ train_script.py
@@ -1,11 +1,14 @@
 import os
 import gymnasium as gym
-from stable_baselines3 import PPO
-from car_racing_modified import CarRacing
-#from car_racing_original import CarRacing
+from stable_baselines3 import PPO, DDPG, DQN, A2C
+#from car_racing_modified import CarRacing
+from car_racing_original import CarRacing
 
-models_dir = "models/PPO"
-logdir = "logs"
+#models_dir = "models/PPO"
+models_dir = "models/PPO_Original"
+logdir = "logs_original"
+#logdir = "logs"
+
 
 if not os.path.exists(logdir):
     os.makedirs(logdir)
@@ -13,21 +16,28 @@ if not os.path.exists(logdir):
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
-'''
+
 # Create the environment
 env = CarRacing()
 env.reset()
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir)
-TIMESTEPS = 1000
-for i in range (1,31):
+
+# Load the latest model if it exists, or start a new one
+model_path = f"{models_dir}/260000.zip"  # Update this with the path to your last saved model
+if os.path.exists(model_path):
+    print("Loading the saved model...")
+    model = PPO.load(model_path, env=env)  # Make sure to pass the environment when loading
+else:
+    print("No saved model found, starting a new model...")
+    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=logdir)
+TIMESTEPS = 100000
+for i in range (1,11):
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
-    model.save(f"{models_dir}/{i * TIMESTEPS}")
+    model.save(f"{models_dir}/{0 + i * TIMESTEPS}")
 
 '''
-
 env = CarRacing(render_mode="human")
 env.reset()
-model_path = f"{models_dir}/30000.zip"
+model_path = f"{models_dir}/700000.zip"
 model = PPO.load(model_path, env=env)
 
 episodes = 10
@@ -40,3 +50,5 @@ for ep in range(episodes):
         obs, reward, done,_, info = env.step(action)
 
 env.close()
+
+'''
